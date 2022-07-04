dcaa9670e25b18778e83be13a20d0f4472fdead4
callmehorhe <91099985+callmehorhe@users.noreply.github.com>
2022-07-04 14:58:27 +0300

    add log levels

--- logger.go
+++ logger.go
@@ -24,6 +24,19 @@ func (l *Logger) SetFormatter(formatter log.Formatter) {
 	l.entry.Logger.SetFormatter(formatter)
 }
 
+const (
+	// ErrorLevel level. Logs. Used for errors that should definitely be noted.
+	// Commonly used for hooks to send errors to an error tracking service.
+	ErrorLevel log.Level = iota + 2
+	// WarnLevel level. Non-critical entries that deserve eyes.
+	WarnLevel
+	// InfoLevel level. General operational entries about what's going on inside the
+	// application.
+	InfoLevel
+	// DebugLevel level. Usually only enabled when debugging. Very verbose logging.
+	DebugLevel
+)
+
 func (l *Logger) SetLevel(level log.Level) {
 	l.entry.Logger.SetLevel(level)
 }
